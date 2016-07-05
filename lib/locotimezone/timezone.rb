4210a42aabdb78c0a112ec631f9f100d6bc946fa
Alex Miller <apmiller108@yahoo.com>
2016-07-04 20:45:47 -0400

    Pass latitude/longitude to Google Timezone API. Attach response to get_loco object.

--- lib/locotimezone/timezone.rb
+++ lib/locotimezone/timezone.rb
@@ -12,9 +12,16 @@ class Timezone
 
   def timezone_query_url
     'https://maps.googleapis.com/maps/api/timezone/json' + '?key=' + @key + 
-      latitude_longitude
+      '&location=' + latitude_longitude + '&timestamp=' + timestamp
   end
 
   def latitude_longitude
+    lat_lng = []
+    @location[:location].each { |k, v| lat_lng.push v.to_s }
+    lat_lng.join(',')
+  end
+
+  def timestamp
+    Time.now.to_i.to_s
   end
 end
