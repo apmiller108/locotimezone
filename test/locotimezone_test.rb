dfb42d077290a01b7857239ba9a10208bfce719a
Alex Miller <apmiller108@yahoo.com>
2016-07-08 17:20:31 -0400

    Modify test to ensure correct location and timezone data is getting returned.

--- test/locotimezone_test.rb
+++ test/locotimezone_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class LocotimezoneTest < Minitest::Test
+  attr_reader :loco
+
   def test_that_it_has_a_version_number
     refute_nil ::Locotimezone::VERSION
   end
 
-  def test_that_it_has_location
-    loco = Locotimezone.locotime address
-    refute_nil loco[:location]
+  def setup
+    @loco = Locotimezone.locotime address
   end
 
-  def test_that_it_has_formatted_address
-    loco = Locotimezone.locotime address
-    refute_nil loco[:formatted_address]
+  def test_that_it_has_correct_formatted_address
+    assert_equal '525 NW 1st Ave, Fort Lauderdale, FL 33301, USA', 
+      loco[:formatted_address]
   end
 
-  def test_that_it_has_timezone_date
-    loco = Locotimezone.locotime address
-    refute_nil loco[:timezone]
+  def test_that_it_has_correct_geolocation
+    assert_equal 26.1288238, loco[:location]['lat']
+    assert_equal -80.1449743, loco[:location]['lng']
+  end
+
+  def test_that_it_has_correct_timezone_id
+    assert_equal 'America/New_York', loco[:timezone]['timeZoneId']
   end
 end
