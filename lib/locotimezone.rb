ecb6cd7f261f04b93c305a048428898dd9196b88
Alex Miller <apmiller108@yahoo.com>
2016-07-11 10:07:51 -0400

    Change option from insecure to secure with a default value of true

--- lib/locotimezone.rb
+++ lib/locotimezone.rb
@@ -8,10 +8,10 @@ module Locotimezone
   def self.locotime(address = '525 NW 1st Ave FL 33301', options = {})
     location_only = options.fetch(:location_only, false)
     timezone_only = options.fetch(:timezone_only, false)
-    insecure      = options.fetch(:insecure, false)
+    secure        = options.fetch(:secure, true)
     key           = options.fetch(:key, ENV['GOOGLE_API_KEY'])
     LocoTime.new(address: address, location_only: location_only,
-                timezone_only: timezone_only, insecure: insecure, key: key)
+                timezone_only: timezone_only, secure: secure, key: key)
       .transform
   end
 end
