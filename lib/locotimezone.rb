36f08437ce78490321063f15c69cc997ea60ff7a
Alex Miller <apmiller108@yahoo.com>
2016-07-16 16:14:26 -0400

    Refactor test to only run api call once per test run

--- lib/locotimezone.rb
+++ lib/locotimezone.rb
@@ -9,7 +9,8 @@ module Locotimezone
     location_only = options.fetch(:location_only, false)
     timezone_only = options.fetch(:timezone_only, false)
     address       = options.fetch(:address, '525 NW 1st Ave FL 33301')
-    location      = options.fetch(:location, { lat: '26.1288238', lng: '-80.1449743' })
+    location      = options.fetch(:location, { lat: '26.1288238', 
+                                               lng: '-80.1449743' })
     secure        = options.fetch(:secure, true)
     key           = options.fetch(:key, ENV['GOOGLE_API_KEY'])
 
--- test/locotimezone_test.rb
+++ test/locotimezone_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
 class LocotimezoneTest < Minitest::Test
-  attr_reader :locotime, :just_location, :just_timezone
+  
+  class << self
+    attr_accessor :locotime, :just_location, :just_timezone
+  end
 
   def setup
-    @locotime = Locotimezone.locotime address: address
-    @just_location = Locotimezone.locotime address: address, location_only: true
-    @just_timezone = Locotimezone.locotime location: lat_lng, timezone_only: true
+    LocotimezoneTest.locotime = LocotimezoneTest.locotime || 
+      Locotimezone.locotime(address: address)
+    LocotimezoneTest.just_location = LocotimezoneTest.just_location || 
+      Locotimezone.locotime(address: address, location_only: true)
+    LocotimezoneTest.just_timezone = LocotimezoneTest.just_timezone || 
+      Locotimezone.locotime(location: lat_lng, timezone_only: true)
   end
 
   def test_that_it_has_a_version_number
@@ -15,25 +21,25 @@ class LocotimezoneTest < Minitest::Test
 
   def test_that_it_has_correct_formatted_address
     assert_equal '525 NW 1st Ave, Fort Lauderdale, FL 33301, USA', 
-      locotime[:geo][:formatted_address]
+      self.class.locotime[:geo][:formatted_address]
   end
 
   def test_that_it_has_correct_geolocation
-    assert_equal 26.1288238, locotime[:geo][:location][:lat]
-    assert_equal -80.1449743, locotime[:geo][:location][:lng]
+    assert_equal 26.1288238, self.class.locotime[:geo][:location][:lat]
+    assert_equal -80.1449743, self.class.locotime[:geo][:location][:lng]
   end
 
   def test_that_it_has_correct_timezone_id
-    assert_equal 'America/New_York', locotime[:timezone][:timezone_id]
+    assert_equal 'America/New_York', self.class.locotime[:timezone][:timezone_id]
   end
 
   def test_that_it_only_returns_location
-    refute_nil just_location[:geo]
-    assert_nil just_location[:timezone]
+    refute_nil self.class.just_location[:geo]
+    assert_nil self.class.just_location[:timezone]
   end
 
   def test_that_it_only_returns_timezone
-    assert_nil just_timezone[:geo]
-    refute_nil just_timezone[:timezone]
+    assert_nil self.class.just_timezone[:geo]
+    refute_nil self.class.just_timezone[:timezone]
   end
 end
