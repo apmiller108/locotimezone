737e4a6b3358ec44ac6b14dfafdeefa0e6115c48
Alex Miller <apmiller108@yahoo.com>
2016-09-02 18:11:02 -0400

    Test active record helper.  Ensure attributes are set for geolocation and timezone.

--- lib/locotimezone/active_record_helper.rb
+++ lib/locotimezone/active_record_helper.rb
@@ -2,9 +2,17 @@ module Locotimezone
   module ActiveRecordHelper
    def locotime(options = {})
      data = Locotimezone.locotime(options)
-     self.latitude = data[:geo][:location][:lat]
-     self.longitude = data[:geo][:location][:lng]
-     self.timezone_id = data[:timezone][:timezone_id]
+     update_geo_attributes data[:geo] unless data[:geo].nil?
+     update_timezone_attribute data[:timezone] unless data[:timezone].nil?
+   end
+
+   def update_geo_attributes(geo_data)
+     self.latitude = geo_data[:location][:lat]
+     self.longitude = geo_data[:location][:lng]
+   end
+
+   def update_timezone_attribute(timezone_data)
+     self.timezone_id = timezone_data[:timezone_id]
    end
   end
 end
---
+++ test/locotimezone_active_record_helper_test.rb
@@ -0,0 +1,61 @@
+require 'test_helper'
+
+class Record < Struct.new(:latitude, :longitude, :timezone_id)
+  include Locotimezone::ActiveRecordHelper
+
+  def save(options)
+    locotime options
+  end
+end
+
+class LocotimezoneActiveRecordHelperTest < Minitest::Test
+
+  describe 'sets location and timezone attributes' do 
+    set_configuration
+    it 'sets latitude' do
+      record = Record.new
+      record.save({ address: address })
+
+      refute_nil record.latitude
+    end
+
+    it 'sets longitude' do
+      record = Record.new
+      record.save({ address: address })
+
+      refute_nil record.longitude
+    end
+
+    it 'sets timezone_id' do
+      record = Record.new
+      record.save({ address: address })
+
+      refute_nil record.timezone_id
+    end
+  end
+
+  describe 'sets location and skips timezone' do
+    set_configuration
+    it 'sets latitude' do
+      record = Record.new
+      record.save({ address: address, skip: :timezone })
+
+      refute_nil record.latitude
+    end
+
+    it 'sets longitude' do
+      record = Record.new
+      record.save({ address: address, skip: :timezone })
+
+      refute_nil record.longitude
+    end
+
+    it 'sets timezone_id' do
+      record = Record.new
+      record.save({ address: address, skip: :timezone })
+
+      assert_nil record.timezone_id
+    end
+
+  end
+end
