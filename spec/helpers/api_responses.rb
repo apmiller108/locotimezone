1c7478a40312db497063b827044dc2e73859131a
Alex Miller <apmiller108@yahoo.com>
2017-08-18 16:37:49 -0400

    Convert locotimezone test to rspec spec

--- spec/helpers/api_responses.rb
+++ spec/helpers/api_responses.rb
@@ -1,7 +1,17 @@
 module ApiResponses
   BASE_PATH = %w[spec helpers api_responses].freeze
+
   def valid_geolocation_response
-    path = File.join(*BASE_PATH, 'valid_geolocation_response.json')
+    read_file File.join(*BASE_PATH, 'valid_geolocation_response.json')
+  end
+
+  def valid_timezone_response
+    read_file File.join(*BASE_PATH, 'valid_timezone_response.json')
+  end
+
+  private
+
+  def read_file(path)
     File.read(path, &:read)
   end
 end
