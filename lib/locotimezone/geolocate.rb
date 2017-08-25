22e6cab563075ddb94597b065cdd92389b5c146f
Alex Miller <apmiller108@yahoo.com>
2017-08-25 14:49:31 -0400

    Extract formatting results to ResultsFormatter class

--- lib/locotimezone/geolocate.rb
+++ lib/locotimezone/geolocate.rb
@@ -7,30 +7,20 @@ module Locotimezone
     end
 
     def call
-      response = open(geolocation_query_url) { |f| JSON.parse f.read }
+      ResultsFormatter.build_geolocation_hash_for geolocation
     rescue OpenURI::HTTPError
       {}
-    else
-      format_results response
     end
 
     private
 
+    def geolocation
+      open(geolocation_query_url) { |f| JSON.parse f.read }
+    end
+
     def geolocation_query_url
       "https://maps.googleapis.com/maps/api/geocode/json?address="\
       "#{address}&key=#{Locotimezone.configuration.google_api_key}"
-    end
-
-    def format_results(response)
-      return {} if response['results'].empty?
-      Hash[
-        formatted_address: response['results'][0]['formatted_address'],
-        location: symbolize_keys(response['results'][0]['geometry']['location'])
-      ]
-    end
-
-    def symbolize_keys(response)
-      response.map { |k, v| [k.to_sym, v] }.to_h
     end
   end
 end
--- lib/locotimezone/results_formatter.rb
+++ lib/locotimezone/results_formatter.rb
@@ -14,5 +14,17 @@ module Locotimezone
         timezone_name: timezone['timeZoneName']
       }
     end
+
+    def self.build_geolocation_hash_for(location)
+      return {} if location['results'].empty?
+      {
+        formatted_address: location['results'][0]['formatted_address'],
+        location: symbolize_keys(location['results'][0]['geometry']['location'])
+      }
+    end
+
+    def self.symbolize_keys(hash)
+      hash.map { |key, value| [key.to_sym, value] }.to_h
+    end
   end
 end
